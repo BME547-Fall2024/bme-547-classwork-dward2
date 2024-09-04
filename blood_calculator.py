f3c60e9e25c61a689f0901e217d60919b1f7629c
David Ward <david.a.ward@duke.edu>
2024-09-04 12:17:38 -0400

    Allow user to select and input HDL value for analysis

--- blood_calculator.py
+++ blood_calculator.py
@@ -7,6 +7,23 @@ def interface():
     print("9 - Quit")
     choice = input("Select a test: ")
     print("You selected {}".format(choice))
+    if choice == "1":
+        hdl_driver()
+
+
+def hdl_driver():
+    # Get the user input
+    hdl_value = hdl_input()   
+    # Compare user input against normal values
+    # Output the results
+    
+    
+def hdl_input():
+    user_input = input("Enter the HDL value: ")
+    user_number = int(user_input)
+    return user_number
+   
+    
     
     
 interface()
