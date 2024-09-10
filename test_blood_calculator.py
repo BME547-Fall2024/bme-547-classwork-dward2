fec9fa568c77af2b7f481027e41898ad52e75b75
David Ward <david.a.ward@duke.edu>
2024-09-10 17:35:23 -0400

    Added testing for LDL analysis

--- test_blood_calculator.py
+++ test_blood_calculator.py
@@ -9,4 +9,15 @@ def test_hdl_analysis(hdl_input, expected):
     answer = hdl_analysis(hdl_input)
     assert answer == expected
     
+
+@pytest.mark.parametrize("ldl_input, expected",
+    [(120, "Normal"),
+     (133, "Borderline High"),
+     (161, "High"),
+     (190, "Very High")])
+def test_ldl_analysis(ldl_input, expected):
+    from blood_calculator import ldl_analysis
+    answer = ldl_analysis(ldl_input)
+    assert answer == expected
+    
     
