a4ccce018a05cdf1e896dc89f3c40755b6f218cc
David Ward <david.a.ward@duke.edu>
2024-09-16 11:53:25 -0400

    Fixed significant figures in output by formatting

--- output_readability.py
+++ output_readability.py
@@ -2,7 +2,7 @@ weight = 20 / 2.205
 dosage = weight * 30
 
 print("CORRECT DOSAGE")
-print("For a patient weighing {} kg,".format(weight))
-print("  the correct dosage is {} mg the first day".format(dosage))
+print("For a patient weighing {:.1f} kg,".format(weight))
+print("  the correct dosage is {:.1f} mg the first day".format(dosage))
 
 
