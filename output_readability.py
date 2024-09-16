4e76192d4b2e225194216959954d53352cd9561b
David Ward <david.a.ward@duke.edu>
2024-09-16 11:48:17 -0400

    Adding file for Merge Conflict demo

---
+++ output_readability.py
@@ -0,0 +1,8 @@
+weight = 20 / 2.205
+dosage = weight * 30
+
+print("CORRECT DOSAGE")
+print("For a patient weighing {} kg,".format(weight))
+print("  the correct dosage is {} mg the first day".format(dosage))
+
+
