a5c6841d4dd862c902f33d44fa0fc7f7bc4a41cf
David Ward <david.a.ward@duke.edu>
2024-09-18 08:59:30 -0400

    Fixed PEP8 errors in database and output_readability

--- database.py
+++ database.py
@@ -14,6 +14,7 @@ etc.
 
 """
 
+
 def make_new_patient(line):
     mrn, test_name, test_value = line.split(",")
     new_patient_dictionary = {"id": int(mrn),
@@ -21,12 +22,13 @@ def make_new_patient(line):
                               "test_value": float(test_value)}
     return new_patient_dictionary
 
+
 def output_db(db):
     for patient in db:
         print("Patient id: {}".format(patient["id"]))
         print("   Test Type: {}".format(patient["test_name"]))
         print("   Test Value: {}".format(patient["test_value"]))
-        
+
 
 def load_data():
     in_file = open("blood_test_data.txt", "r")
@@ -42,14 +44,11 @@ def load_data():
     #         new_patient = line.strip("\n")
     #         print(new_patient)
 
-        
-        
+
 def main():
     db = load_data()
     output_db(db)
- 
-    
+
+
 if __name__ == "__main__":
     main()
-
-    
