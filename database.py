c60673aa0cd844b1b89967c8138f7a58167da4a8
David Ward <david.a.ward@duke.edu>
2024-09-18 08:54:45 -0400

    Functions to create a patient entry, add to database, and print directory to screen

--- database.py
+++ database.py
@@ -1,10 +1,42 @@
+"""
+   patient = {"id": <id_num_as_int>,
+              "test_name": <str>,
+              "test_value": <int or float>}
+
+Patient id: 1553
+   Test Type:  HDL
+   Test Value: 65
+Patient id: 2344
+   Test Type: HDL
+   Test Value: 45
+Patient id: 4427
+etc.
+
+"""
+
+def make_new_patient(line):
+    mrn, test_name, test_value = line.split(",")
+    new_patient_dictionary = {"id": int(mrn),
+                              "test_name": test_name,
+                              "test_value": float(test_value)}
+    return new_patient_dictionary
+
+def output_db(db):
+    for patient in db:
+        print("Patient id: {}".format(patient["id"]))
+        print("   Test Type: {}".format(patient["test_name"]))
+        print("   Test Value: {}".format(patient["test_value"]))
+        
+
 def load_data():
     in_file = open("blood_test_data.txt", "r")
+    db = []
     for line in in_file:
         new_patient = line.strip("\n")
-        print(new_patient)
+        new_patient_dict = make_new_patient(new_patient)
+        db.append(new_patient_dict)
     in_file.close()
-    
+    return db
     # with open("blood_test_data.txt", "r") as in_file:
     #     for line in in_file:
     #         new_patient = line.strip("\n")
@@ -13,8 +45,9 @@ def load_data():
         
         
 def main():
-    load_data()
-    
+    db = load_data()
+    output_db(db)
+ 
     
 if __name__ == "__main__":
     main()
